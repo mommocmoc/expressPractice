77e1ad6196816afcd71ca0e38567551b9aa2ee21
JaeHwan So <mommocmoc@gmail.com>
2020-04-26 19:03:07 +0900

    Express Basic:

---
+++ root/bfkorea.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+var fortunes = [
+    "사랑",
+    "우정",
+    "희망",
+    "건강",
+    "행복"
+]
+//Default Set-up
+app
+.set('port', process.env.PORT || port)
+.set('view engine', 'pug')
+.use(express.static(__dirname + '/public'));
+
+//Router
+app
+//Home
+.get('/', (req, res) => {
+    res.render('main', {
+        title: 'Beautiful Korea Travel',
+        message: 'Welcome to Beautiful Korea Travel!',
+        message2: '아름다운 한국 여행사에 오신 것을 환영합니다!'
+    })
+})
+//About
+.get('/about', (req, res) => {
+    res.render('fortune', {
+        title: 'About',
+        message: 'About BFKorea Travel',
+        message2: fortunes[Math.floor(Math.random()*fortunes.length)]
+    })
+})
+
+//Error Handler
+app
+//404
+.use((req, res, next) => {
+    res.status(400);
+    res.render('main', {
+        title: 'Ooops!! 404',
+        message: 'There is no place to go here...:(',
+        message2: '갈 수 없는 페이지에요... :('
+    })
+})
+//500
+.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render('main', {
+        title: 'Ooops!! 500',
+        message: 'Something Goes Wrong...:(',
+        message2: '잠시 문제가 발생했어요... :('
+    })
+})
+
+
+//Server Begin
+app.listen(app.get('port'), () => {
+    console.log(`Express started on http://localhost:${app.get('port')} ; press Ctrl-C to terminate.`);
+})
